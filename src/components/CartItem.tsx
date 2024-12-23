cbf919e78d65c32da08f2eff30c6c6d41726dde3
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-12-24 02:48:36 +0500

    Add to cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -5,14 +5,22 @@ import Image from "next/image";
 import { IoMdClose } from "react-icons/io";
 import { FaMinus, FaPlus } from "react-icons/fa";
 
-const CartItem = ({ card }: any) => {
-  const { id, image, category, title, price } = card;
+interface CardProps {
+  id: string;
+  image: string;
+  category: string;
+  title: string;
+  price: number;
+  amount: number;
+}
+
+const CartItem = ({ card }: { card: CardProps }) => {
+  const { id, image, category, title, price, amount } = card;
 
   return (
     <div className="flex flex-col  pt-7">
       <div className="w-full min-h-[100px] flex items-center justify-start ">
         <Link href={`/product/${id}`}>
-          
           <Image
             className="w-1/2"
             src={image}
@@ -22,7 +30,7 @@ const CartItem = ({ card }: any) => {
           />
         </Link>
 
-        <div>
+        <div className="flex flex-col w-full">
           <div className=" flex items-center justify-between gap-6 mb-2">
             <Link
               href={`/product/${id}`}
@@ -34,14 +42,19 @@ const CartItem = ({ card }: any) => {
               <IoMdClose className="text-gray-500 cursor-pointer hover:text-red-500 transition text-xl sm:text-2xl lg:text-3xl" />
             </div>
           </div>
-
-          <div className="flex justify-start items-center gap-x-4">
-            <div>
-              <FaMinus />
+          <div className="flex justify-between">
+            <div className="flex justify-start items-center ">
+              <div className="border px-1 sm:px-1 py-[6px] sm:py-2 hover:bg-gray-700 hover:text-white transition-transform">
+                <FaMinus className="text-sm" />
+              </div>
+              <div className="border px-1 sm:px-2 py-[1px] sm:py-1">{amount}</div>
+              <div className="border px-1 sm:px-1 py-[6px] sm:py-2 hover:bg-gray-700 hover:text-white transition-transform">
+                <FaPlus className="text-sm" />
+              </div>
             </div>
-            <div>amount </div>
-            <div>
-              <FaPlus />
+            <div className="flex w-full justify-between px-2 items-center py-2">
+              <div className="text-[12px] sm:text-sm md:text-base">$ {price}</div>
+              <div className="text-[12px] sm:text-sm md:text-base">{`$ ${(price * amount).toFixed(2)}`}</div>
             </div>
           </div>
         </div>
